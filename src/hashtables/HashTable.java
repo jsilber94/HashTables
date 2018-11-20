9db1ae844be50a227548eafc1260114ec22dbc61
Max <mpage-sowik@outlook.com>
2018-11-20 13:20:33 -0500

    fully implemented abstract hastable

--- src/hashtables/HashTable.java
+++ src/hashtables/HashTable.java
@@ -9,6 +9,52 @@ package hashtables;
  *
  * @author Max Page-Slowik
  */
-public class HashTable {
+public abstract class HashTable <E>{
+    private E[] backingData;
     
+    public HashTable(int size){
+        backingData = (E[]) new Object[size];
+    }
+    
+    public int size(){
+        return backingData.length;
+    }
+    
+    public boolean isEmpty(){
+        for (int i = 0; i<size();i++){
+            if(backingData[i]!=null){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public int hashcode(int key){
+        return key % size();
+    }
+    
+    public E get(int k){
+        return backingData[hashcode(k)];
+    }
+    public E put (int k, E v){
+        E data = backingData[hashcode(k)];
+        if (data != null){
+            backingData[hashcode(k)] = v;
+            return data;
+        }
+        else{
+            backingData[hashcode(k)] = v;
+            return null;
+        }
+    }
+    public E remove(int k){
+        E data = backingData[hashcode(k)];
+        if(data != null){
+            backingData[hashcode(k)] = null;
+            return data;
+        }
+        else{
+            return null;
+        }
+    }
 }
