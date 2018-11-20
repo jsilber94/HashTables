2b339244bd8a48b9dc19b8039635f8bbcbccd832
Max <mpage-sowik@outlook.com>
2018-11-20 15:57:31 -0500

    fixing hastable

--- src/hashtables/LinearProbeHash.java
+++ src/hashtables/LinearProbeHash.java
@@ -10,6 +10,8 @@ package hashtables;
  * @author Max Page-Slowik
  */
 public class LinearProbeHash extends HashTable{
+    private int collisions = 0;
+    private int elements = 0;
     
     public LinearProbeHash(int size) {
         super(size);
@@ -18,14 +20,33 @@ public class LinearProbeHash extends HashTable{
     @Override
     public Object put(int k, Object v){
         Object value = super.put(k, v);
+        collisions++;
+        elements++;
+        int attempts = 0; 
         for(int i = (++k); i<super.size() && value!= null;i++){
             value = super.put(i, v);
+            attempts++;    
         }
         if(value != null){
             throw new IndexOutOfBoundsException("No more room in the hashtable");
         }
+        System.out.println("Size: "+super.size());
+        System.out.println("Elements: "+elements);
+        System.out.println("Collisions: "+collisions);
+        System.out.println("Attempts: "+attempts);
         return value;
     }
+
+    @Override
+    public Object remove(int k) {
+        return super.remove(k);
+    }
+
+    @Override
+    public Object get(int k) {
+        return super.get(k); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
     
 }
