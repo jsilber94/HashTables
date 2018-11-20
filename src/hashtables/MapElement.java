53f4e9f4e1f480772a66192918f9ad3130b980fb
Jesse Silber <jessesilber@gmail.com>
2018-11-20 12:54:41 -0500

    made MapElement class

---
+++ src/hashtables/MapElement.java
@@ -0,0 +1,21 @@
+package hashtables;
+
+/**
+ *
+ * @author Jesse
+ */
+public class MapElement {
+
+    int key = 0;
+    String value = "";
+
+    public MapElement() {
+        key = (int) Math.random();
+    }
+
+    public int hashCode() {
+        //implement poly accumulation or unicode
+        //atleast 16bits
+        return 0;
+    }
+}
