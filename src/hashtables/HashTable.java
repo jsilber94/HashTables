269d5bdced71c38089df686cfb33c6f784507973
Max <mpage-sowik@outlook.com>
2018-11-20 13:40:19 -0500

    changed hashtable to implement methods properly

--- src/hashtables/HashTable.java
+++ src/hashtables/HashTable.java
@@ -9,48 +9,49 @@ package hashtables;
  *
  * @author Max Page-Slowik
  */
-public abstract class HashTable <E>{
-    private E[] backingData;
+public abstract class HashTable{
+    private Integer[] hashTable;
     
     public HashTable(int size){
-        backingData = (E[]) new Object[size];
+        hashTable = (Integer[]) new Integer[size];
     }
     
     public int size(){
-        return backingData.length;
+        return hashTable.length;
     }
     
     public boolean isEmpty(){
         for (int i = 0; i<size();i++){
-            if(backingData[i]!=null){
+            if(hashTable[i]!=null){
                 return false;
             }
         }
         return true;
     }
     
-    public int hashcode(int key){
-        return key % size();
+    public int hashcode(int k){
+        return k % size();
     }
     
-    public E get(int k){
-        return backingData[hashcode(k)];
+    public Integer get(int k){
+        return hashTable[hashcode(k)];
     }
-    public E put (int k, E v){
-        E data = backingData[hashcode(k)];
+    
+    public Integer put (int k, Integer v){
+        Integer data = hashTable[hashcode(k)];
         if (data != null){
-            backingData[hashcode(k)] = v;
+            hashTable[hashcode(k)] = v;
             return data;
         }
         else{
-            backingData[hashcode(k)] = v;
+            hashTable[hashcode(k)] = v;
             return null;
         }
     }
-    public E remove(int k){
-        E data = backingData[hashcode(k)];
+    public Integer remove(int k){
+        Integer data = hashTable[hashcode(k)];
         if(data != null){
-            backingData[hashcode(k)] = null;
+            hashTable[hashcode(k)] = null;
             return data;
         }
         else{
